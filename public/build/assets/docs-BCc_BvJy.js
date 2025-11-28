b6eb22c20b8a0f1d67864aaf735bdb7c24700a2b
Romis <romis@nesmelov.com>
2025-11-28 09:47:53 +0300

    fixed print function

---
+++ public/build/assets/docs-BCc_BvJy.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$("#print a").click(function(n){n.preventDefault(),$(this).remove(),window.print()})});
--- resources/views/layouts/docs.blade.php
+++ resources/views/layouts/docs.blade.php
@@ -2,6 +2,7 @@
 <html>
 <head>
     <meta charset="utf-8">
+    <script type="text/javascript" src="{{ asset('js/jquery.js') }}"></script>
     @vite([
         'resources/css/icons/icomoon/styles.css',
         'resources/css/docs.css',
